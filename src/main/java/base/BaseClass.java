7dcb0cf5b813ea663764ad1633cc0902f1bbd87a
Gowthuyadav <gowthuyadav001@gmail.com>
2025-11-20 15:17:30 +0530

    Fix CI workflow: headless Chrome + Firefox

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -1,4 +1,5 @@
 package base;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -6,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
 import java.util.Properties;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
@@ -13,13 +15,12 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
@@ -37,24 +38,20 @@ public class BaseClass {
         p.load(file);
 
         logger = LogManager.getLogger(this.getClass());
-        boolean isCI = System.getenv("CI") != null; // detect GitHub Actions
-
+        boolean isCI = System.getenv("CI") != null;
 
         if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
             if (isCI) options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
             driver = new ChromeDriver(options);
+
         } else if (browser.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
             FirefoxOptions options = new FirefoxOptions();
             if (isCI) options.addArguments("--headless");
             driver = new FirefoxDriver(options);
-        } else if (browser.equalsIgnoreCase("edge")) {
-            WebDriverManager.edgedriver().setup();
-            EdgeOptions options = new EdgeOptions();
-            if (isCI) options.addArguments("--headless");
-            driver = new EdgeDriver(options);
+
         } else {
             throw new IllegalArgumentException("Browser not supported: " + browser);
         }
@@ -67,20 +64,15 @@ public class BaseClass {
 
     @AfterMethod
     public void teardown() {
-        driver.quit();
+        if (driver != null) driver.quit();
     }
 
     public String captureScreen(String tname) throws IOException {
-
         String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
         File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-
-        String destPath = System.getProperty("user.dir") + "/screenshots/"
-                + tname + "_" + timeStamp + ".png";
-
+        String destPath = System.getProperty("user.dir") + "/screenshots/" + tname + "_" + timeStamp + ".png";
         File dest = new File(destPath);
         src.renameTo(dest);
-
         return destPath;
     }
 }
